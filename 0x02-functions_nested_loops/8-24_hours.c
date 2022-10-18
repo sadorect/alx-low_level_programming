066c0ea35457e54ee73f338f5ce5860c373c35cf
Tosin Omojola <sadorect@gmail.com>
2022-10-18 13:41:48 -0700

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,21 +2,21 @@
 
 /**
  * jack_bauer - Prints every minute of the day of
- *              Jack Bauer, starting from 00:00 to 23:59.
+ *              Jack Bauer (the Federal Agent ), starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
-	int hour, minute;
+	int hour, minutes;
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		for (minutes = 0; minutes <= 59; minutes++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((minute / 10) + '0');
-			_putchar((minute % 10) + '0');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
 			_putchar('\n');
 		}
 	}
