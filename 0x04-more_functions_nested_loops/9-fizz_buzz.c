0cc83179a9822a43ba7da04459c202460f33ad20
sadorect <sadorect@gmail.com>
2022-10-20 15:13:31 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,10 +3,9 @@
 
 /**
  * main - entry point
- * Description: prints the numbers 1 - 100 
+ * Description: prints the numbers 1 - 100
  * Substitute Fizz for multiples of 3, Buzz for multiples of 5,
  * and FizzBuzz for multiples of both 3 and 5 ie. 15
- *
  * Return: always 0
  */
 int main(void)
